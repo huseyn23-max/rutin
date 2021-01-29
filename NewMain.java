3032095dc48a72613f7dec56f74155b161cf4bba
huseyn23-max <78222437+huseyn23-max@users.noreply.github.com>
2021-01-29 16:56:45 +0100

    Add files via upload

---
+++ NewMain.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package newpackage;
+
+/**
+ *
+ * @author Husey
+ */
+public class NewMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+//      String name = "Qabil";
+//      int age = 31;
+//      int tel = 693172;
+//      String mail = "qabil3169@gmail.com";
+//      System.out.println("Name " + name + "\nAge " + age + "\nTel " + tel + "\nMail " + mail);
+//      
+//      String name1 = "Maqa";
+//      int age1 = 18;
+//      int tel1 = 8392048;
+//      String mail1 = "maqaqaqa72@gmail.com";
+//      
+//      System.out.println("Name: " + name1 + "\nAge: " + age1 + "\nTel: " + tel1 + "\nMail: " + mail1);
+//      
+      int beginn = 5;
+      int endet = 10;
+      int dauer = endet-beginn ;
+      
+      System.out.println("Beginn: " + beginn + "\nEndet: " + endet + "\nInsgesamt: " + dauer);
+      
+      
+        
+        
+        
+        
+    }
+    
+}
